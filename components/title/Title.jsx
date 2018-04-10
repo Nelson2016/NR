eec21b63179b12e42ed330aa96175270fd35c714
Nelson <Nelson_Lee@outlook.com>
2018-04-10 18:24:32 +0800

    [BUG FIX] Repair title component error.
    Repair title component error.

--- components/title/Title.jsx
+++ components/title/Title.jsx
@@ -7,7 +7,7 @@ class Title extends React.Component {
     render() {
         return <div className={styles['n-title']}>
             <h1 className={styles["n-title-text"]}>{this.props.title}</h1>
-            {this.props.more.text && <span className={styles["n-title-more"]}
+            {this.props.more && this.props.more.text && <span className={styles["n-title-more"]}
                                            onClick={this.props.more.onClick.bind(this)}>{this.props.more.text}</span>
             }
         </div>
